e8e6204ecebf3991f89586e13b8747919efcf260
Aditya Kumar <aditya.kuumar.cg@gmail.com>
2026-09-08 11:25:46 +0530

    kohli kohli

---
+++ condition.py
@@ -0,0 +1,322 @@
+# present=input("enter your percentage: ")
+# if present>"30":
+#     print("you are pass")
+#     marks=input("enter your marks in maths: ")
+#     if marks>"50":
+#         print("you are pass in maths")
+#         college =input("enter your college preference like (a,b,c): ") 
+#         if college=="a":
+#             print("you are selected in college a")
+#         if college=="b" or college=="c":
+#             print("you are selected in college b or c")
+# if present<"30":
+#     print("you are fail")
+#     if present<"30":
+#         print("you are fail in maths")
+# type=input("enter your gender (m/f): ")
+# if type=="m":
+#     print("you are male")
+#     age=input("enter your age: ")
+#     if age>="18":
+#         print("you are male  adult")
+#     else:
+#         print("you are not adult")
+# else:
+#     print("you are female")
+#     input("enter your age: ")
+#     if age>="18":
+#         print("you are female adult")
+#     else:
+#         print("you are not adult")
+# type=input("enter your gender (m/f): ")
+# if type=="m":
+#     print("you are male")
+# else:
+#     if type=="f":   
+#         print("you are female")
+#     else:
+#         print("type valid input")
+
+#1
+# log = int(input(" enter any number : "))
+# if  log>10 :
+#     print("greater than 10")
+# else :
+#     if log==10:
+#         print("equal to 10")
+#     else :
+#         print("less than 10")
+
+# #2
+# age=int(input("Enter your age : "))
+# if age>=18 :
+#     print ("You are adult")
+# else :
+#     print("You are not adult")
+
+#3
+# number= int(input("Enter any number : "))
+# if number>0 :
+#     print("Number is : positive" )
+# else :
+#     print("Number is : negative")
+
+#4
+ 
+# print("operations :")
+# print("1.addition ")
+# print("2.subtraction ")
+# print("3.multiplication ")
+# print("4.divison")
+# operation=input("enter operation you want to perform like:1,2,3,4 :")
+# a=int(input("Enter a number : " ))
+# b=int(input("Enter another number : "))
+# if operation=="1":
+#     print( f"addition is : {a+b)"}
+# elif operation=="2" :
+#     print(f"subtraction is :{a-b}")
+# elif operation=="3" :
+#     print(f"multiplication is :{a*b}")
+# else :
+#      if operation=="4":
+#          print(f"divison is :{a/b}")
+#      else :
+#          print("Enter valid operation ")
+#5
+# number=input("Enter any number :")
+# if number=="0" :
+#     print("zero")
+# else :
+#     print("not zero")
+#6
+# lockin=int(input("enter a number :"))
+# if lockin>0 :
+#     print ("positive")
+# else :
+#     print("not positive")
+#7
+# age=int(input("enter your age :"))
+# if age>=18 : 
+#     print("adult")
+# else :
+#     print("minor")
+#8
+# number=int(input("Enter any number :"))
+# if number%2 == 0 :
+#     print ("number is even")
+# else :
+#     print("number is odd")
+# #9
+# marks=int(input("Enter your marks : "))
+# if marks<40 :
+#     print ("you are fail")
+# else :
+#     print("you are pass")
+#10
+# a=int(input("enter a number : "))
+# b=int(input("enter another number : "))
+# if a>b :
+#     print(f"{a} is greater than {b}")
+# else :
+#     print("greater number is : " + str(a))
+#11
+# marks=int(input("Enter your marks : "))
+# if marks>=90 :
+#     print("A")
+# elif marks>=75 :
+#     print("B")
+# elif marks>=60 :
+#     print("c")
+# elif marks>=40 :
+#     print("D")
+# else :
+#     print("F")
+#12
+# number=int(input("Enter a number : "))
+
+# if number>0 :
+#     print ("positive")
+# elif number<0:
+#     print("not positive")
+# else :
+#     print("equal to zero")
+#13
+# print("day :")
+# print("1.monday ")
+# print("2.tuesday")
+# print("3.wednesday ")
+# print("4.thursday")
+# print("5.friday")
+# operation=input("enter day you want like:1,2,3,4,5 :")
+
+
+# if operation=="1":
+#     print( "day is  monday")
+# elif operation=="2" :
+#     print("day is tuesday")
+# elif operation=="3" :
+#     print("day is wednesday")
+# elif operation=="4":
+#     print("day is thursday")
+# elif operation=="5" :
+#     print("day is friday")
+# else :
+#     print("other")
+#14
+# marks=int(input("Enter your marks : "))
+# if marks>=90 :
+#      print("excellent")
+# elif marks>=75 :
+#      print("good")
+# elif marks>=60 :
+#      print("pass")
+# elif marks>=40 :
+#     print("poor")
+# else :
+#     print("Fail")
+#15
+# number=int(input("Enter a number like 1,2,3 : "))
+# if number==1 :
+#     print("1")
+# elif number==2 :
+#     print("2")
+# elif number==3 :
+#     print("3")
+# else :
+#     print("other")
+#16
+# age =int(input("Enter your age : "))
+# if age>=18 :
+#     if age<=60 :
+#         print("between 18 and 60")
+# else :
+#     print("not applicable")
+#17
+# marks =int(input("Enter your marks : "))
+# if marks >=40 :
+#     if marks >=75 :
+#         print("good")
+#     else :
+#         print("passed")
+# else :
+#     print("fail")
+#18
+# num = int(input("Enter a number: "))
+
+# if num > 0:
+#     if num > 100:
+#         print("Number is positive and greater than 100")
+#     else:
+#         print("Number is positive but not greater than 100")
+# else:
+#     print("Number is not positive")
+
+#19
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     if age >= 60:
+#         print("You are a senior citizen")
+#     else:
+#         print("You are an adult")
+# else:
+#     print("You are a minor")
+
+#20
+# num = int(input("Enter a number: "))
+
+# if num != 0:
+#     if num > 0:
+#         print("Number is positive")
+#     else:
+#         print("Number is negative")
+# else:
+#     print("Number is zero")
+
+#21
+# marks =int(input("Enter your marks : "))
+# age =int(input("Enter your age : "))
+# if age >= 18 and marks >=40 :
+#     print ("eligible")
+# else :
+#     print("not eligible")
+
+#22
+# number=int(input("Enter your number : "))
+# if number < 10 or number > 100 :
+#     print("special")
+# else :
+#     print("casual")
+
+#23
+# age =int(input("Enter your age : "))
+
+# if age >= 18 and has_id is True:
+#     print("Allowed")
+# else :
+#     print("not allowed")
+
+#24
+# first_number = int(input("Enter first number: "))
+# second_number = int(input("Enter second number: "))
+
+# if first_number > 10 and second_number > 10:
+#     print("Both are greater than 10")
+
+#25
+# number = int(input("Enter a number: "))
+
+# if number < 0 or number > 100:
+#     print("Number is either less than 0 or greater than 100")
+
+#26
+# is_closed= not input( "Enter True or  False : ")
+# if  is_closed == False:i
+#     print("open")
+# else :
+#     print("close")
+
+#27
+# number = int(input("Enter a number: "))
+
+# if number >= 10 and number <= 50:
+#     print("Number is between 10 and 50")
+
+#28
+# number = int(input("Enter a number: "))
+
+# if number < 10 or number > 50:
+#     print("Number is outside the range 10 to 50")
+ 
+#29
+# is_student = True
+# has_id = True
+# has_ticket = True
+
+# if is_student and has_id and has_ticket:
+#     print("Allowed")
+
+#30
+# age =int(input("Enter your age : "))
+# marks =int(input("Enter your marks : "))
+# has_id=  input( "Enter True or  False : ").capitalize
+# if age>=18 and marks>=40 and has_id == True :
+#     print ("eligible")
+# else :
+#     print("not eligible")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
