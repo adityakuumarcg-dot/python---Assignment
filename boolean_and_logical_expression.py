9f580ba2c4807aca5857d09e3cb0d284e9b8c051
Aditya Kumar <aditya.kuumar.cg@gmail.com>
2026-09-08 11:28:37 +0530

    virat virat

---
+++ boolean_and_logical_expression.py
@@ -0,0 +1,71 @@
+#2
+print(type(True))
+
+#3
+print(type(False))
+
+#6
+print(10==10)
+
+#7
+print(10!=5)
+
+#8
+print(5 > 10)
+
+#9
+print(5 < 10)
+
+#10
+print(18 >= 18)
+
+#11
+print(18 <= 10)
+
+#13
+10 == 10
+10 != 5
+10 > 5
+5 < 10
+10 >= 10
+5 <= 10
+
+#15
+age = 20
+print(age >= 18)
+# #answer will be True
+
+#16
+marks = 45
+print(marks == 50)
+##answer will be False
+
+#17
+age = 20
+print(age >= 18)
+
+#22
+age = 25
+print(age >= 18 and age <= 60)
+## answer is True
+
+#23
+age = 16
+print(age < 18 or age > 60)
+#answer is True
+
+number = int(input("Enter a number: "))
+# Q25
+if number > 10 and number < 50:
+    print("Number is greater than 10 and less than 50")
+
+# Q26
+if number < 10 or number > 50:
+    print("Number is less than 10 or greater than 50")
+
+# Q27
+if not (number > 10 and number < 50):
+    print("Number is NOT between 10 and 50")
+
+
+
---
+++ new.py
@@ -0,0 +1,24 @@
+a ="aditya"
+print(a)
+print(a[0:3])
+print(a[3:6])
+# a=input("enter your birth date : ")
+# b=input("enter your birth month:")
+# c=input("enter your birth year:")
+# print("your birth date is:", a, b, c, sep="/")
+# a=int(input("enter your number:"))
+
+# if a%2==0:
+#     print("your number is even")
+# if a%2==1:
+#     print("your number is odd")
+# school=input("enter your name of school:")
+# if school == "sainik school":
+#     print ("your school is best")
+# if school != "sainik school":
+#     print ("your school is  mid ")
+# print (type(school))
+lego=input ("enter your full name: ")
+a,b = lego.split()
+print("your first name is:",a)
+print("your last name is :",b)
